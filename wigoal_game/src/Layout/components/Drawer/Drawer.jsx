bed1854bf9b4c4f5ed6c3ead45d4425a58f60d17
15021677496 <CNFH6678>
2023-11-28 12:08:06 +0800

    update

--- wigoal_game/src/Layout/components/Drawer/Drawer.jsx
+++ wigoal_game/src/Layout/components/Drawer/Drawer.jsx
@@ -2,7 +2,7 @@ import React, {useState, useRef} from 'react';
 import {useNavigate} from 'react-router-dom';
 
 import {SearchOutlined} from "@ant-design/icons";
-import {Drawer, Tabs } from "antd";
+import {Button, Drawer, Tabs} from "antd";
 import './Drawer.less'
 
 import Login from "./Login"
@@ -22,30 +22,48 @@ const DrawerContainer = (props) => {
 
 
     return (
-        <Drawer className="container_drawer" placement="right" onClose={onClose} open={openSwitch}>
-            <svg preserveAspectRatio="none" height="170" width="100%" viewBox="0 0 375 170" fill="none" xmlns="http://www.w3.org/2000/svg"
-                 style={{
-                     position: 'absolute',
-                     zIndex: '-1',
-                     left: '0',
-                     top: '0',
-                 }}
-            >
-                <path d="M0 0H375V125.5L295.199 162.826C286.526 166.883 277.007 168.805 267.439 168.432L0 158V0Z" fill="#6842FF"></path>
-            </svg>
+        <Drawer className="container_drawer black"
+                title={tabsActive == '2' ? 'Create your CrazyGames account for free!' : ''}
+                placement="right"
+                size={'large'}
+                onClose={onClose}
+                open={openSwitch}>
+            {
+                tabsActive == '2'
+                    ? <svg preserveAspectRatio="none" height="170" width="100%" viewBox="0 0 375 170" fill="none"
+                           xmlns="http://www.w3.org/2000/svg"
+                           style={{
+                               position: 'absolute',
+                               zIndex: '-1',
+                               left: '0',
+                               top: '0',
+                           }}
+                    >
+                        <path d="M0 0H375V125.5L295.199 162.826C286.526 166.883 277.007 168.805 267.439 168.432L0 158V0Z"
+                              fill="#6842FF"></path>
+                    </svg>
+                    : ''
+            }
+
             <Tabs
                 defaultActiveKey={['1']}
                 activeKey={tabsActive}
                 items={[
                     {
-                        key: '1',
-                        label: 'Sign up',
-                        children: <Signup />,
+                        key: '2',
+                        label: <Button shape="round" size="large" style={{ color: '#2f3148',}}>Sign up</Button>,
+                        children: <Signup/>,
                     },
                     {
-                        key: '2',
-                        label: 'Log in',
-                        children: <Login />,
+                        key: '1',
+                        label: <Button shape="round" size="large"
+                                       style={{
+                                            backgroundColor: 'transparent',
+                                            borderColor: '#d9d9d9',
+                                            color: '#d9d9d9',
+                                        }}
+                                >Log in</Button>,
+                        children: <Login/>,
                     },
                 ]}
                 onTabClick={onTabClick}
