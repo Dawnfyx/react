4eac01672b38375f201776dc8aafc287bb06f487
15021677496 <CNFH6678>
2023-12-13 15:56:08 +0800

    update video

--- wigoal_video/src/pages/Play/Play.jsx
+++ wigoal_video/src/pages/Play/Play.jsx
@@ -12,21 +12,7 @@ import { Pagination } from 'swiper/modules';
 const PlayPage = ({ count }) => {
 
     return (
-        // <div className="play_box">
-        //     <h1>{count}</h1>
-        //     <video controls style={{width: '100%', height: '100%'}}>
-        //         <source src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.webm" type="video/webm" />
-        //
-        //         <source src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4" type="video/mp4" />
-        //
-        //         Download the
-        //         <a href="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.webm">WEBM</a>
-        //         or
-        //         <a href="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4">MP4</a>
-        //         video.
-        //     </video>
-        // </div>
-        <>
+        <div className="play_box">
             <Swiper
                 direction={'vertical'}
                 pagination={{
@@ -45,7 +31,7 @@ const PlayPage = ({ count }) => {
                 <SwiperSlide>Slide 8</SwiperSlide>
                 <SwiperSlide>Slide 9</SwiperSlide>
             </Swiper>
-        </>
+        </div>
     )
 }
 
