9563ebaeefcbcfb8245f90d2a5da02fc2aa7db9f
15021677496 <CNFH6678>
2024-01-06 18:48:15 +0800

    update

--- wigoal_game/src/Layout/components/Content/GameCarousel/GameCarouselNew.jsx
+++ wigoal_game/src/Layout/components/Content/GameCarousel/GameCarouselNew.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import {Link} from "react-router-dom";
 
 // Import Swiper React components
@@ -21,10 +21,26 @@ const GameCarousel = (props) => {
 
     const { viewData } = props;
 
+    const swiperRef = useRef();
+    const [mousemoveFlag, setMousemoveFlag] = useState(false);
+
+    const handleTouchMove = () => {
+        // swiperLinkRef.current.addEventListener("mouseenter", (el)=>{
+        //     console.log('mouseenter', el)
+        //     setMousemoveFlag(true)
+        // });
+        // swiperLinkRef.current.addEventListener("mouseleave", (el)=>{
+        //     console.log('mouseleave', el)
+        //     setMousemoveFlag(false)
+        // });
+        console.log('sssss')
+    }
+
     return (
         <>
 
             <Swiper
+                ref={swiperRef}
                 slidesPerView={6}
                 spaceBetween={10}
                 centeredSlides={false}
@@ -43,10 +59,33 @@ const GameCarousel = (props) => {
                     viewData.map((items, keys) => (
                         <SwiperSlide key={keys}>
                             <Link className="swiper-slide-link" to={"/page/details?gid=" + items.gid}>
-                                <img width="100%" height="100%" src={'http://test.ads-goal.com' + items.icon} />
-                                <div className="hoverContainer">
-                                    <img src="" alt=""/>
-                                </div>
+                                <img width="100%" height="100%" src={'http://test.ads-goal.com' + items.icon}  />
+                                {
+                                    mousemoveFlag
+                                    ? <>
+                                        <div className="hoveredContainer">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 251 150" fill="none"
+                                                 style={{width: '100%'}}>
+                                                <path d="M0 0L125.5 16.3983L251 0V43H0V0Z" fill="#8668FF"></path>
+                                                <path d="M0 26L125.5 42.7797L251 26V70H0V26Z" fill="#7755FF"></path>
+                                                <path d="M0 53L125.5 69.3983L251 53V96H0V53Z" fill="#6842FF"></path>
+                                                <path d="M0 79L125.5 95.7797L251 79V123H0V79Z" fill="#5E3BE6"></path>
+                                                <path d="M0 107L125.5 123.398L251 107V150H0V107Z" fill="#5335CC"></path>
+                                            </svg>
+                                            <div className="buttonContainer">
+                                                <img src={'http://test.ads-goal.com' + items.icon} alt="Holey.io Battle Royale" />
+                                                <span>{items.name}</span>
+                                                <button className="MuiButton-root" type="button">Play</button>
+                                            </div>
+                                        </div>
+
+                                        <div className="hoverContainer">
+                                            <img src={'http://test.ads-goal.com' + items.icon} alt=""/>
+                                        </div>
+                                    </>
+                                    : ''
+                                }
+
                             </Link>
                         </SwiperSlide>
                     ))
